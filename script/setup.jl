919ba23fa4150fedaa458a620212c7e23af6184b
alanaguo <131525354+alanaguo@users.noreply.github.com>
2025-11-10 10:33:59 +0800

    Update setup.jl

--- script/setup.jl
+++ script/setup.jl
@@ -3,7 +3,7 @@ using DrWatson
 import Pkg
 @quickactivate "UBItelematics-jl"
 
-# use 'using Pkg Pkg.add()' to add dependent packages
+# use 'using Pkg; Pkg.add()' to add dependent packages
 
 # locate R for RCall.jl
 # ENV["R_HOME"] = "path/to/yout/R"
