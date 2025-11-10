d76d38046a9c124820403f2ed7942086de782cbd
alanaguo <131525354+alanaguo@users.noreply.github.com>
2025-11-10 21:07:47 +0800

    Update and rename MainModule to MainModule.jl

---
+++ src/MainModule.jl
@@ -0,0 +1,42 @@
+module UBItelematics
+
+import Base: sum, size, length, zeros, 
+import Base: ifelse, +, -, *, ==, /, ^
+import Base: hcat, vcat, fill, reshape
+
+using DataFrames
+import DataFrames: DataFrame
+
+using CategoricalArrays
+import CategoricalArrays: levels
+
+using StatsModels
+import StatsModels: schema, apply_schema, modelcols, coefnames
+
+using Statistics  
+import Statistics: quantile
+
+using Distributions
+import Distributions: Normal, pdf, cdf
+
+# using PACKAGE
+# import PACKAGE: FUNCTION, FUNCTION, ...
+
+export safe_divide,
+       compute_catg_prop,
+       compute_catg_sum,
+       cooks_distance,
+
+       generate_LRMoE_data,
+       LRMoE_CIp,
+       
+       # MODULE DEFINED FUNCTIONS
+
+include("common_utils")
+# include all source files (under src/)
+
+
+
+UBItelematics
+
+end # module
